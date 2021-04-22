a6e46dabddb424b5e284e5ab2bd26048a87768e2
Ali Ma <ali.ma@sjsu.edu>
2021-04-22 16:50:47 -0700

    Added and connected for Sign Up screen

---
+++ GoodHearted/signUpViewController.swift
@@ -0,0 +1,119 @@
+//
+//  signUpViewController.swift
+//  GoodHearted
+//
+//  Created by Ali Ma on 4/22/21.
+//
+
+import UIKit
+import Parse
+
+private let reuseIdentifier = "Cell"
+
+class signUpViewController: UIViewController {
+
+    
+    @IBOutlet weak var userFullNameField: UITextField!
+    @IBOutlet weak var userEmailField: UITextField!
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var userPasswordField: UITextField!
+    @IBOutlet weak var userPhoneNumberField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        //self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func backFromSignUpButton(_ sender: Any) {
+    }
+    @IBAction func submitSignUpButton(_ sender: Any) {
+        let user = PFUser()
+        user.username = userNameField.text!
+        user.password = userPasswordField.text!
+        user.email = userEmailField.text!
+        // other fields can be set just like with PFObject
+//        user["phone"] = userPhoneNumberField.text
+        
+        user.signUpInBackground{(success, error) in
+            if success{
+                self.performSegue(withIdentifier: "signUpSegue", sender: nil)
+            } else{
+                print("Error: signUpNotSuccessful")
+            }
+            
+        }
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+//    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+//
+//
+//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of items
+//        return 0
+//    }
+//
+//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+//
+//        // Configure the cell
+//
+//        return cell
+//    }
+//
+//    // MARK: UICollectionViewDelegate
+//
+//    /*
+//    // Uncomment this method to specify if the specified item should be highlighted during tracking
+//    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+//        return true
+//    }
+//    */
+//
+//    /*
+//    // Uncomment this method to specify if the specified item should be selected
+//    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+//        return true
+//    }
+//    */
+//
+//    /*
+//    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+//    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+//        return false
+//    }
+//
+//    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+//        return false
+//    }
+//
+//    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+//
+//    }
+//    */
+//
+}

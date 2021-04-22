c8ce5da41abbd530a4199c1b2f2c3e6002fb0c79
Truc Phan <pttruc.308@gmail.com>
2021-04-21 21:34:27 -0700

    Add Emergency Contacts Screen

---
+++ GoodHearted/AddEmergencyViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AddEmergencyViewController.swift
+//  GoodHearted
+//
+//  Created by Truc Phan on 4/20/21.
+//
+
+import UIKit
+import Parse
+
+class AddEmergencyViewController: UIViewController {
+    @IBOutlet weak var ecFullNameField: UITextField!
+    @IBOutlet weak var ecEmailField: UITextField!
+    @IBOutlet weak var ecPhoneField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func messageSwitch(_ sender: Any) {
+    }
+    @IBAction func callSwitch(_ sender: Any) {
+    }
+    @IBAction func addButton(_ sender: Any) {
+        guard let user = PFUser.current() else {
+            print("Fail to get user")
+            return
+        }
+        do {
+            user["fullName"] = ecFullNameField.text
+            user.email = ecEmailField.text
+            user["phone"] = ecPhoneField.text
+            try user.save()
+        } catch {
+            print(error)
+        }
+    }
+    @IBAction func laterButton(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil);
+        let vc = storyboard.instantiateViewController(withIdentifier: "HomeScreen");
+        self.present(vc, animated: true, completion: nil);
+    }
+}
---
+++ GoodHearted/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  GoodHearted
+//
+//  Created by Truc Phan on 4/19/21.
+//
+
+import UIKit
+
+@IBDesignable extension UIButton {
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

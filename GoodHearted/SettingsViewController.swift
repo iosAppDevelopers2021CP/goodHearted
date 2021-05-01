a1942e4438c9ec64ee61e0d0bf6be3d9d3885095
Ali Ma <ali.ma@sjsu.edu>
2021-05-01 13:57:21 -0700

    Added Navigation "back button" item on Settings screen

---
+++ GoodHearted/SettingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsViewController.swift
+//  GoodHearted
+//
+//  Created by Ali Ma on 4/29/21.
+//
+
+import UIKit
+
+class SettingsViewController: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

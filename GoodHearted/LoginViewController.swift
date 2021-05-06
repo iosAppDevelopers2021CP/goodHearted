95ae38d4c4054fca1303cab8631eebcbd76057d3
Truc Phan <pttruc.308@gmail.com>
2021-05-06 00:00:39 -0700

    update sign in

--- GoodHearted/LoginViewController.swift
+++ GoodHearted/LoginViewController.swift
@@ -10,9 +10,20 @@ import Parse
 
 class LoginViewController: UIViewController {
     
-    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var userPasswordField: UITextField!
     
-    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = userPasswordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print(error!)
+            }
+        }
+    }
     
 }

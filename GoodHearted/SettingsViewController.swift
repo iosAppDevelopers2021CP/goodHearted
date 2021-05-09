dfab9a42c9b890f7d99f116b618cc223f1b054fc
Ali Ma <ali.ma@sjsu.edu>
2021-05-08 23:51:45 -0700

    Sign Out button work successfully

--- GoodHearted/SettingsViewController.swift
+++ GoodHearted/SettingsViewController.swift
@@ -2,18 +2,34 @@
 //  SettingsViewController.swift
 //  GoodHearted
 //
-//  Created by Ali Ma on 4/29/21.
+//  Created by Ali Ma on 5/8/21.
 //
 
 import UIKit
+import Parse
 
-class SettingsViewController: UICollectionView {
+class SettingsViewController: UIViewController {
+   // let user = PFUser()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignOut(_ sender: Any) {
+        PFUser.logOut()
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
     */
 

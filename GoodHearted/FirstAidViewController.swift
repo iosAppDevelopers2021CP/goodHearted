63273bb1845d46be7ff69b07048d1b9320b80e10
Ali Ma <ali.ma@sjsu.edu>
2021-06-10 13:05:23 -0700

    Added and Modified First-Aid Booklet Screens

---
+++ GoodHearted/FirstAidViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstAidViewController.swift
+//  GoodHearted
+//
+//  Created by Ali Ma on 6/8/21.
+//
+
+import UIKit
+
+class FirstAidViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
